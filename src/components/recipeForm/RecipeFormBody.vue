c0b5a1cbd757a3b66ff27831a6570946bec7b72d
Muhammad Iqbal Baiduri Yamani <mikbalby@gmail.com>
2025-06-17 21:34:17 +0700

    Fix Error

--- src/components/recipeForm/RecipeFormBody.vue
+++ src/components/recipeForm/RecipeFormBody.vue
@@ -1,75 +1,93 @@
 <script setup>
-    import BaseInput from '../ui/BaseInput.vue';
-    import BaseSelect from '../ui/BaseSelect.vue';
-    import BaseTextArea from '../ui/BaseTextArea.vue';
-    import BaseButton from '../ui/BaseButton.vue';
+import { ref, reactive, watchEffect, onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useStore } from 'vuex'
 
-    import { onMounted, reactive } from "vue";
-    import { ref } from 'vue';
-    import { useRoute, useRouter } from 'vue-router';
-    // Impor store Pinia Anda
-    import { useRecipeStore } from '@/store/recipe';
-    
-    // Gunakan store Pinia
-    const recipeStore = useRecipeStore();
-    const router = useRouter();
-    const route = useRoute();
+import BaseInput from '../ui/BaseInput.vue'
+import BaseSelect from '../ui/BaseSelect.vue'
+import BaseTextArea from '../ui/BaseTextArea.vue'
+import BaseButton from '../ui/BaseButton.vue'
 
-    const props = defineProps({
-        isEdit: { type: Boolean, default: false },
-        detailData: { type: Object },
-    });
+const store = useStore()
+const router = useRouter()
+const route = useRoute()
 
-    const recipeData = reactive({
-        imageLink: "",
-        name: "",
-        description: "",
-        category: "",
-        prepTime: 0,
-        cookTime: 0,
-        totalTime: 0,
-        ingredients: [],
-        directions: [],
-    });
+const props = defineProps({
+  isEdit: { type: Boolean, default: false },
+  detailData: { type: Object },
+})
 
-    onMounted(async () => {
-        if (props.isEdit) {
-            try {
-                await recipeStore.getRecipeDetail(route.params.id);
-                Object.assign(recipeData, recipeStore.recipeDetail);
-                
-                ingredientCount.value = recipeStore.recipeDetail.ingredients?.length || 1;
-                directionCount.value = recipeStore.recipeDetail.directions?.length || 1;
-            } catch (err) {
-                console.log(err);
-            } 
-        }
-    });
+// Data untuk form
+const form = ref({ title: '' })
 
-    const ingredientCount = ref(1);
-    const directionCount = ref(1);
+// Data lengkap resep
+const recipeData = reactive({
+  imageLink: '',
+  name: '',
+  description: '',
+  category: '',
+  prepTime: 0,
+  cookTime: 0,
+  totalTime: 0,
+  ingredients: [],
+  directions: [],
+})
 
-    const addIngredient = () => { ingredientCount.value++; };
-    const addDirection = () => { directionCount.value++; };
-    const deleteIngredient = (index) => {
-        recipeData.ingredients.splice(index, 1);
-        ingredientCount.value--;
-    };
-    const deleteDirection = (index) => {
-        recipeData.directions.splice(index, 1);
-        directionCount.value--;
-    };
+const ingredientCount = ref(1)
+const directionCount = ref(1)
 
-    const totalTime = () => {
-        recipeData.totalTime = parseInt(recipeData.prepTime || 0) + parseInt(recipeData.cookTime || 0);
-    };
+const addIngredient = () => { ingredientCount.value++ }
+const addDirection = () => { directionCount.value++ }
+const deleteIngredient = (index) => {
+  recipeData.ingredients.splice(index, 1)
+  ingredientCount.value--
+}
+const deleteDirection = (index) => {
+  recipeData.directions.splice(index, 1)
+  directionCount.value--
+}
+const totalTime = () => {
+  recipeData.totalTime = parseInt(recipeData.prepTime || 0) + parseInt(recipeData.cookTime || 0)
+}
 
-    const addNewRecipe = async () => {
-        if (props.isEdit){
-            await recipeStore.updateRecipe({ id: route.params.id, newRecipe: recipeData });
-        } else {
-            await recipeStore.addNewRecipe(recipeData);
-        }
-        router.push("/user/user-recipe");
-    };
+// Sync form data saat edit
+watchEffect(() => {
+  if (props.isEdit && props.detailData) {
+    form.value = { ...props.detailData }
+  }
+})
+
+// Load detail resep saat edit
+onMounted(async () => {
+  if (props.isEdit) {
+    try {
+      await store.dispatch('recipe/getRecipeDetail', route.params.id)
+      Object.assign(recipeData, store.state.recipe.recipeDetail)
+
+      ingredientCount.value = recipeData.ingredients?.length || 1
+      directionCount.value = recipeData.directions?.length || 1
+
+      // Sync juga form.title jika perlu
+      form.value.title = recipeData.name || ''
+    } catch (err) {
+      console.log(err)
+    }
+  }
+})
+
+const onSubmit = async () => {
+  if (props.isEdit) {
+    await store.dispatch('recipe/updateRecipe', { id: route.params.id, newRecipe: recipeData })
+  } else {
+    await store.dispatch('recipe/addNewRecipe', recipeData)
+  }
+  router.push('/user/user-recipe')
+}
 </script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <input name="title" v-model="form.title" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
